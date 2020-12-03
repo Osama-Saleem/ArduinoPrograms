f4950c890baf27009da0c773f83055eb5428d3cd
Osama-Saleem <75374175+Osama-Saleem@users.noreply.github.com>
2020-12-03 18:52:11 +0500

    Add files via upload

---
+++ one_serial.ino
@@ -0,0 +1,29 @@
+char a;
+void setup() {
+  
+pinMode(13,OUTPUT);
+
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ if(Serial.available()){
+ a=Serial.read();
+ }
+
+ if(a=='1'){
+  digitalWrite(13,HIGH);
+  Serial.println(a);
+ }
+  else if(a=='2'){
+ 
+  digitalWrite(13,LOW);
+  Serial.println(a);
+ }
+ else {
+   Serial.println("noooo");
+  } 
+delay(1000);
+
+}
