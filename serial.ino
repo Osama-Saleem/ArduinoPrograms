a153badf2452289dd2a76bc87bbf9163fd06d58e
Osama-Saleem <75374175+Osama-Saleem@users.noreply.github.com>
2020-12-04 16:56:38 +0500

    Add files via upload

---
+++ serial.ino
@@ -0,0 +1,53 @@
+bool a;
+bool b;
+bool c;
+void setup() {
+  pinMode(4,INPUT);
+  pinMode(5,INPUT);
+  pinMode(6,INPUT);
+Serial.begin(9600);
+}
+
+void loop() { 
+a=digitalRead(4);
+b=digitalRead(5);
+c=digitalRead(6);
+ if(a==HIGH && b==HIGH && c==HIGH )
+{
+  Serial.println("no buttons are pressed");
+  delay(1000);
+  }
+  else if(a==HIGH && b==LOW && c==LOW )
+{
+  Serial.println("two buttons are pressed");
+  delay(1000);
+  }
+  else if(a==LOW && b==HIGH && c==LOW )
+{
+  Serial.println("two buttons are pressed");
+  delay(1000);
+  }
+  else if(a==LOW && b==LOW && c==HIGH )
+{
+  Serial.println("two buttons are pressed");
+  delay(1000);
+  }
+  else if(a==HIGH && b==HIGH && c==LOW )
+{
+  Serial.println("one buttons are pressed");
+  delay(1000);
+  }
+  else if(a==LOW && b==HIGH && c==HIGH )
+{
+  Serial.println("one buttons are pressed");
+  delay(1000);
+  }
+  else if(a==HIGH && b==LOW && c==HIGH )
+{
+  Serial.println("one buttons are pressed");
+  delay(1000);
+  }
+  else
+  Serial.println("three buttons are pressed");
+  delay(1000);
+  }
---
+++ seven_segment_with_button.ino
@@ -0,0 +1,161 @@
+bool a;
+int i=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(12,OUTPUT);//a
+pinMode(11,OUTPUT);//b
+pinMode(10,OUTPUT);//c
+pinMode(9,OUTPUT);//d
+pinMode(3,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  a= digitalRead(3);
+if(a==HIGH && i==0){
+ digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(9,LOW);
+ i++;
+ a=LOW;
+}
+else if(a==HIGH && i==1){
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+  }
+else if(a==HIGH && i==2){
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==3){
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==4){
+   digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,LOW);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==5){
+   digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,LOW);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==6){
+  digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==7){
+   digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==8){
+   digitalWrite(12,HIGH);
+ digitalWrite(11,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==9){
+   digitalWrite(12,HIGH);
+ digitalWrite(11,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==10){
+   digitalWrite(12,HIGH);
+ digitalWrite(11,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==11){
+   digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==12){
+  digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==13){
+   digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,LOW);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==14){
+   digitalWrite(12,LOW);
+ digitalWrite(11,HIGH);
+ digitalWrite(10,LOW);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==15){
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,HIGH);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==16){
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,HIGH);
+ digitalWrite(9,LOW);
+ i++;
+  a=LOW;
+}
+else if(a==HIGH && i==17){
+  digitalWrite(12,LOW);
+ digitalWrite(11,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(9,HIGH);
+ i=0;
+  a=LOW;
+  }
+
+delay(500);
+}
