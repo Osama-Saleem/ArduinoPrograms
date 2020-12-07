5e309b4617d5e4661097c4ace00b338d537d8748
Osama-Saleem <75374175+Osama-Saleem@users.noreply.github.com>
2020-12-07 19:35:17 +0500

    Add files via upload
    Design a temperature control chamber for bakery room. There are two temperature sensor which provide output voltage from 20mV to 2V range temperature from 20C to 80C. Two fans also install in chamber for air cooling of chamber set temperature must 50C chamber. If temperature raise above 50C both fans should turn ON to stable temperature.

---
+++ temperature_condition.ino
@@ -0,0 +1,26 @@
+int x;
+int y;
+float a;
+float b;
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+x = analogRead(A0);
+y = analogRead(A4);
+a=(5/1023.0)*x;
+b=(5/1023.0)*y;
+
+
+if (a>0.50 && b>0.50){
+  digitalWrite(13,HIGH);
+  digitalWrite(12,HIGH);
+}
+else {
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+}
+}
